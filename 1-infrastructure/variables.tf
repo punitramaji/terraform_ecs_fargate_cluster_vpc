1b294fd308d8c013b3d2e5d9a3b931f9469f0e27
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 02:18:53 +0530

    add variable public_subnet_2_cidr

--- 1-infrastructure/variables.tf
+++ 1-infrastructure/variables.tf
@@ -11,3 +11,8 @@ variable "vpc_cidr" {
 variable "public_subnet_1_cidr" {
   description = "Public Subnet 1 CIDR"
 }
+
+
+variable "public_subnet_2_cidr" {
+  description = "Public Subnet 2 CIDR"
+}
