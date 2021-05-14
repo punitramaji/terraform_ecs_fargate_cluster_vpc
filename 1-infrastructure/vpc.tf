b36351a8d511973dc517ffbe3b20f33773d411f0
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 04:00:27 +0530

    fixed typo enable_dns_hostnames

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -8,7 +8,7 @@ terraform {
 
 resource "aws_vpc" "production-vpc" {
   cidr_block          = "${var.vpc_cidr}"
-  enable_dns_hostname = true
+  enable_dns_hostnames = true
   
   tags {
     name = "Production-vpc"
