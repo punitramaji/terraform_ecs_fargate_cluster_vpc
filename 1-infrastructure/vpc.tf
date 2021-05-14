9039325bf798dcf9834cc9ab6d152dffa70ffd59
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 02:43:45 +0530

    private route table association to private subnets

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -106,3 +106,18 @@ resource "aws_route_table_association" "public-subnet-3-association" {
   route_table_id = "${aws_route_table.public-route-table.id}"
   subnet_id      = "${aws_subnet.public-subnet-3.id}"
 }
+
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3.id}"
+}
