b56e19a1a5b8afcd697b1c5cb9dcf053f28ec25c
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 04:21:41 +0530

    fixed typo of aws_subnet.public-subnet-1.id for nat-gw

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -133,7 +133,7 @@ resource "aws_eip" "elastic-ip-for-nat-gw" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
-  subnet_id     = "{aws_subnet.public-subnet-1.id}"
+  subnet_id     = "${aws_subnet.public-subnet-1.id}"
   
   tags = {
     Name = "{aws_eip.elastic-ip-for-nat-gw}"
