672edc63305ad11e3804218209fbf75db5e6db17
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 02:17:59 +0530

    public subnet 2 added

--- 1-infrastructure/vpc.tf
+++ 1-infrastructure/vpc.tf
@@ -15,10 +15,20 @@ resource "aws_vpc" "production-vpc" {
   }
 }
 
-resource "aws_subnet" "" {
+resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
   vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "eu-west-1a"
+  
+  tags {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "public-subnet-2" {
+  cidr_block        = "${var.public_subnet_2_cidr}"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
+  availability_zone = "eu-west-1b"
   
   tags {
     Name = "Public-Subnet-1"
