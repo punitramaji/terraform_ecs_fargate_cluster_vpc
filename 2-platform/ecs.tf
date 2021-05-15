7c7479756e07e7c2de65de5594c13e76b78af50a
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 12:26:31 +0530

    create data to read the remote infra status

--- 2-platform/ecs.tf
+++ 2-platform/ecs.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "${var.region}"
 }
+
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "s3"
+  
+  config {
+    region = "${var.region}"
+    bucket = "${}"
+  }
+}
