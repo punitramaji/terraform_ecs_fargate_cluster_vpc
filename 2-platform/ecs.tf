435c3753a2e7f59ee57d927b85ce3c7472d19f0c
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 13:43:44 +0530

    Create route 53 record for load balancer

--- 2-platform/ecs.tf
+++ 2-platform/ecs.tf
@@ -34,3 +34,17 @@ resource "aws_alb" "ecs_cluster_elb" {
     Name = "${var.ecs_cluster_name}-ALB"
   }
 }
+
+
+#Create route 53 record for load balancer
+resource "aws_route53_record" {
+  name    = "*.${var.ecs_domain_name}"
+  type    = "A"
+  zone_id = "${data.aws_route53_zone.ecs_domain.zone_id}"
+  
+  alias {
+    evaluate_target_health = false
+    name                   = "${aws_alb.ecs_cluster_alb.dns_name}"
+    zone_id                = "${aws_alb.ecs_cluster_alb.zone_id}"
+  }
+}
