892be2fbaf11866ddd265f222f428a3195226c5a
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 13:36:12 +0530

    add actual certificate validation request

--- 2-platform/domain.tf
+++ 2-platform/domain.tf
@@ -22,3 +22,8 @@ resource "aws_route53_record" "ecs_cert_validation_record" {
   ttl             = 60
   allow_overwrite = true
 }
+
+resource "aws_acm_certificate_validation" "ecs_domain_certificate_validation" {
+  certificate_arn = "${aws_acm_certificate.ecs_domain_certificate.arn}"
+  validation_record_fqdns = ["${aws_route53_record.ecs_cert_validation_record.fqdn}"]
+}
