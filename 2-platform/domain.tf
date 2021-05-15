a8d5e360c4a437f368df938140e175bcb5176e8c
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 13:12:55 +0530

    add validation method and tag to acm. completes certificate creation

--- 2-platform/domain.tf
+++ 2-platform/domain.tf
@@ -1,5 +1,10 @@
 #Create domain certificates that we are using with load balancer of clutser and add validation
 #domain name starts with * is because we use same domain for our subdomain also
 resource "aws_acm_certificate" "ecs_domain_certificate" {
-  domain_name = "*.${var.ecs_domain_name}"
+  domain_name       = "*.${var.ecs_domain_name}"
+  validation_method = "DNS"
+  
+  tags = {
+    Name = "${var.ecs_cluster_name}-Certificate"
+  }
 }
