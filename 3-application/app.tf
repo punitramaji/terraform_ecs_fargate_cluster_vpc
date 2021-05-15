6bacf26e501c7dc63e4f2298a4da8f3b30e61341
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 19:00:30 +0530

    add data source to read backend s3

--- 3-application/app.tf
+++ 3-application/app.tf
@@ -2,3 +2,14 @@ provider "aws" {
   region = "${var.region}"
 }
 
+terraform {
+  backend "s3" {}
+}
+
+data "terraform_remote_state" "platform" {
+  backend = "s3"
+  
+  config = {
+    key = "${var.remote_state_key}"
+  }
+}
