4184ef399971eb67af3e943d404c0f411fd30d12
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 15:33:46 +0530

    Update outputs.tf

--- 2-platform/outputs.tf
+++ 2-platform/outputs.tf
@@ -14,3 +14,22 @@ output "ecs_cluster_name" {
   value = "${aws_ecs_cluster.production-fargate-cluster.name}"
 }
 
+output "ecs_cluster_role_name" {
+  value = "${aws_iam_role.ecs_cluster_role.name}"
+}
+
+output "ecs_cluster_role_arn" {
+  value = "${aws_iam_role.ecs_cluster_role.arn}"
+}
+
+output "ecs_domain_name" {
+  value = "${var.ecs_domain_name}"
+}
+
+output "ecs_public_subnets" {
+  value = "${data.terraform_remote_state.infrastructure.public_subnets}"
+}
+
+output "ecs_pricate_subnets" {
+  value = "${data.terraform_remote_state.infrastructure.private_subnets}"
+}
