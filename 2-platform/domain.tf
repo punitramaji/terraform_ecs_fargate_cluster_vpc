57d954de980672f80155078f7828aaa46bf88091
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 16:45:34 +0530

    Update domain.tf

--- 2-platform/domain.tf
+++ 2-platform/domain.tf
@@ -18,7 +18,7 @@ resource "aws_route53_record" "ecs_cert_validation_record" {
   name            = "${aws_acm_certificate.ecs_domain_certificate.domain_validation_options.0.resource_record_name}"
   type            = "${aws_acm_certificate.ecs_domain_certificate.domain_validation_options.0.resource_record_type}"
   zone_id         = "${data.aws_route53_zone.ecs_domain.zone_id}"
-  records         = ["${aws_acm_cretificate.ecs_domain_certificate.domain_validation_options.0.resource_record_value}"]
+  record          = ["${aws_acm_cretificate.ecs_domain_certificate.domain_validation_options.0.resource_record_value}"]
   ttl             = 60
   allow_overwrite = true
 }
