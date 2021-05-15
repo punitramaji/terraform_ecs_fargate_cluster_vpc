2d1faa02cf0f8875937b20724e3a19d8c6830cba
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 13:32:00 +0530

    create route53 record for domain name validation

--- 2-platform/domain.tf
+++ 2-platform/domain.tf
@@ -14,3 +14,11 @@ data "aws_route53_zone" "ecs_domain" {
   private_zone = false
 }
 
+resource "aws_route53_record" "ecs_cert_validation_record" {
+  name            = "${aws_acm_certificate.ecs_domain_certificate.domain_validation_options.0.resource_record_name}"
+  type            = "${aws_acm_certificate.ecs_domain_certificate.domain_validation_options.0.resource_record_type}"
+  zone_id         = "${data.aws_route53_zone.ecs_domain.zone_id}"
+  records         = ["${aws_acm_cretificate.ecs_domain_certificate.domain_validation_options.0.resource_record_value}"]
+  ttl             = 60
+  allow_overwrite = true
+}
