3eecf140e011782f93aa22d427536b8cfba4616f
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 12:50:31 +0530

    add egress and ingress to security group, completes SG creation

--- 2-platform/securitygroups.tf
+++ 2-platform/securitygroups.tf
@@ -4,9 +4,16 @@ resource "aws_security_group" "ecs_alb_security_group" {
   vpc_id      = "${data.terraform_remote_state.infrastructure.vpc_id}"
   
   ingress {
-    from_port = 443
-    protocol = "TCP"
-    to_port = 443
-    cidr_blocks = []
+    from_port   = 443
+    protocol    = "TCP"
+    to_port     = 443
+    cidr_blocks = ["${var.internet_cidr_blocks}"]
+  }
+  
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["${var.internet_cidr_blocks}"]
   }
 }
