8b50484d75c48f6e35049638a23f9854d95f2b28
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 20:14:56 +0530

    Update app.tf

--- 3-application/app.tf
+++ 3-application/app.tf
@@ -46,14 +46,16 @@ resource "aws_iam_role" "fargate_iam_role" {
   name               = "${var.ecs_service_name}-IAM-Role"
   assume_role_policy = <<EOF
 {
-	"Version": "2012-10-17",
-	"Statement": [{
-		"Effect": "Allow",
-		"Principal": {
-			"Service": ["ecs.amazonaws.com", "ecs-tasks.amazonaws.com"]
-		},
-		"Action": "sts:AssumeRole"
-	}]
+"Version": "2012-10-17",
+"Statement": [
+  {
+    "Effect": "Allow",
+    "Principal": {
+       "Service": ["ecs.amazonaws.com", "ecs-tasks.amazonaws.com"]
+    },
+    "Action": "sts:AssumeRole"
+  }
+  ]
 }
 EOF
 }
