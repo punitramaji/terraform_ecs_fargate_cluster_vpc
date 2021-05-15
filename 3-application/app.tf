bae11e28747d92b34c49d1c88f81771331b8d9a3
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 20:23:30 +0530

    create fargate_iam_role_policy

--- 3-application/app.tf
+++ 3-application/app.tf
@@ -59,3 +59,27 @@ resource "aws_iam_role" "fargate_iam_role" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "fargate_iam_role_policy" {
+  name = "${var.ecs_service_name}-IAM-Role-Policy"
+  role = "${aws_iam_role.fargate_iam_role.id}"
+  
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:*",
+        "ecr:*",
+        "logs:*",
+        "cloudwatch:*",
+        "elasticloadbalancing:*",
+      ],
+      "Resource" "*"
+     }
+  ]
+}
+EOF
+}
