b3e32bd25dcfd514bb534c2ad2641186f25eb825
Punit Ramaji <punitramaji@users.noreply.github.com>
2021-05-15 13:08:54 +0530

    create acm certificate

---
+++ 2-platform/domain.tf
@@ -0,0 +1,5 @@
+#Create domain certificates that we are using with load balancer of clutser and add validation
+#domain name starts with * is because we use same domain for our subdomain also
+resource "aws_acm_certificate" "ecs_domain_certificate" {
+  domain_name = "*.${}"
+}
